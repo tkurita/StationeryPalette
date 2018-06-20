621424a895d247e532c949245c5c615895ebb91b
Tetsuro Kurita <tkurita@mac.com>
2018-06-20 19:19:10 +0900

    remove Localize Tools

--- LocalizeTools/localize_ib.mk
+++
@@ -1,6 +0,0 @@
-# following variables must be given as command line arguments
-#target_lang = Japanese
-#target_nib = Japanese.lproj/MainMenu.nib
-
-$(target_nib): $(target_lang).lproj/%.nib: en.lproj/%.nib $(target_lang).lproj/%.strings
-	./LocalizeTools/localize_ib.sh $< $@
